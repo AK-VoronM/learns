dc8a5dfba872587aae7a64cb01f8c251a0abba6e
AK-VoronM <ak.alz@mail.ru>
2016-01-11 23:46:50 +0300

    Удаление админ функций
    Удаление кнопки "добавить нового пользователя" из пользовательской части
    сайта

--- views/footer.php
+++ views/footer.php
@@ -4,7 +4,6 @@
             <?php if ($this->user_group == 1): ?>
             <ul>
                 <li><a href="/learns/?ctrl=Users&act=ShowUsers">Список пользователей</a></li>
-                <li><a href="/learns/?ctrl=Users&act=AddUser">Добавить пользователя</a></li>
             </ul>
             <?php endif; ?>
         </div>
