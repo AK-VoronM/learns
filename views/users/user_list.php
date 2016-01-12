243ace0de093b825f9e1c5aa5ffcb5ba7363d910
MartinScorsese <eduardzoz@yandex.ru>
2016-01-12 22:39:34 +0300

    CSS стили + таблица (не доделано)

--- views/users/user_list.php
+++ views/users/user_list.php
@@ -1,5 +1,7 @@
 <ul>
-    <li>user_id user_login user_group</li>
+<div class="usersul">user_id</div>
+<div class="usersul">user_login</div>
+<div class="usersul">user_group</div>
 <?php foreach ($this->users as $user):?>
     <li><?php echo $user->user_id, ' ', $user->user_login, ' ', $user->user_group; ?></li>
 <?php endforeach; ?>
