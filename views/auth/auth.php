4980cf113c775d25d6601a538df179e4f37d88b6
AK-VoronM <ak.alz@mail.ru>
2016-01-12 19:24:38 +0300

    Рамка с предупреждением
    Рамка на главную + стили для рамки

--- views/auth/auth.php
+++ views/auth/auth.php
@@ -1,13 +1,17 @@
 <form method="post" class="forminput" action="?ctrl=Auth&act=Login" role="form">
     <ul>
-            <li><label for="inputEmail">Login:</label></li>
+            <li><label for="inputEmail">Логин:</label></li>
             <li><input type="text" maxlength="50" id="login" name="login"></li>
     </ul>
     <ul>
-            <li><label for="inputPass">Password:</label></li>
+            <li><label for="inputPass">Пароль:</label></li>
             <li><input type="password" id="password" name="password"></li>
     </ul>
     <ul>
             <li><input type="submit" class="btn" value="Войти"></li>
     </ul>
 </form>
+
+<div class="regramka">
+    Для регистрации в системе обращайтесь по адресу: ak.alz@mail.ru.
+</div>
