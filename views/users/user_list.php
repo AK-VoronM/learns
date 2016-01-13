1e9511d742b48baf651ff8b43152fb46cf83e901
AK-VoronM <ak.alz@mail.ru>
2016-01-13 10:10:41 +0300

    Изменения в разметке + новый стиль
    Изменил рабметку в файле + добавил стиль clear
    
    А вообще это кривая реализзация ))) и нужно копать через список.

--- views/users/user_list.php
+++ views/users/user_list.php
@@ -1,8 +1,10 @@
 <div class="usersul">user_id</div>
 <div class="usersul">user_login</div>
 <div class="usersul">user_group</div>
+<div class="clear"></div>
 <?php foreach ($this->users as $user):?>
 <div class="usersul"><?php echo $user->user_id; ?></div>
 <div class="usersul"><?php echo $user->user_login; ?></div>
 <div class="usersul"><?php echo $user->user_group; ?></div>
+<div class="clear"></div>
 <?php endforeach; ?>
