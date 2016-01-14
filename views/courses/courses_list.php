7adf9d720c0c2422e0685f4b7b62016b4141eb18
MartinScorsese <eduardzoz@yandex.ru>
2016-01-14 20:34:21 +0300

    Блоки курсов (два в ряд) + новые стили.

--- views/courses/courses_list.php
+++ views/courses/courses_list.php
@@ -7,17 +7,14 @@
 <hr>
 
 <?php foreach ($this->courses as $course): ?>
-<div class="courses_block">
-  <a href="" onClick="Page.Go(this.href); return false">
+<div id="listcourses">
     <img src="{ava}" class="courses_block_avatar">
-  </a>
     <div class="courses_name"><?php echo $course->course_name; ?></div>
     <div class="courses_desc"><?php echo $course->course_shortdescription; ?></div>
-    <div class="clear" style="height:0px;"></div>
+    <div class="clear"></div>
     <!-- Место для даты -->
-    <div class="clear" style="height:15px;"></div>
-    <button class="courses_button">Читать</button>
-    <div class="clear" style="height:15px;"></div> 
-  </div>
+    <div class="clear"></div>
+    <button class="courses_button" href="/learns/?ctrl=Lessons&act=ShowLessons&id=<?php echo $course->course_id; ?>">Читать</button>
+    <div class="clear"></div> 
 </div>
 <?php endforeach; ?>
