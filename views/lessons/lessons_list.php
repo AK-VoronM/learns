003ac01c55780e71da56f21b39a65b2a2330685b
MartinScorsese <eduardzoz@yandex.ru>
2016-01-18 21:52:37 +0300

    Блоки уроков с использованием стилей курсов

--- views/lessons/lessons_list.php
+++ views/lessons/lessons_list.php
@@ -9,3 +9,19 @@
     </div>
     </a>
 </div>
+
+
+<hr>
+
+<?php foreach ($this->lessons as lesson): ?>
+<div id="listcourses">
+    <img src="{ava}" class="courses_block_avatar">
+    <div class="courses_name"><?php echo $lesson->lesson_name; ?></div>
+    <div class="courses_desc"><?php echo $lesson->lesson_shortdescription; ?></div>
+    <div class="clear"></div>
+    <!-- Место для даты -->
+    <div class="clear"></div>
+    <button class="courses_button" href="/learns/?ctrl=Lessons&act=ShowLesson&id=<?php echo $lesson->lesson_id; ?>">Читать</button>
+    <div class="clear"></div> 
+</div>
+<?php endforeach; ?>
