fa26edbeac50d81b4903bc31f79a49e633f8dd13
MartinScorsese <eduardzoz@yandex.ru>
2016-01-19 19:56:13 +0300

    Переход на другую страницу button

--- views/lessons/lesson_view.php
+++ views/lessons/lesson_view.php
@@ -6,8 +6,8 @@ $course = $this->course;
 <h3>из курса: <a href="/learns/?ctrl=Lessons&act=ShowLessons&id=<?php echo $course->course_id; ?>"><?php echo $course->course_name; ?></a></h3>
 <p><?php echo $lesson->lesson_text; ?></p>
 <?php if($this->lesson_prew): ?>
-<button href="/learns/?ctrl=Lessons&act=ShowLesson&id=<?php echo $this->lesson_prew->lesson_id; ?>"><?php echo $this->lesson_prew->lesson_name; ?></button>
+<a href="/learns/?ctrl=Lessons&act=ShowLesson&id=<?php echo $this->lesson_prew->lesson_id; ?>"><button><?php echo $this->lesson_prew->lesson_name; ?> </button></a>
 <?php endif; ?>
 <?php if($this->lesson_next): ?>
-<button href="/learns/?ctrl=Lessons&act=ShowLesson&id=<?php echo $this->lesson_next->lesson_id; ?>"><?php echo $this->lesson_next->lesson_name; ?> </button>
+<a href="/learns/?ctrl=Lessons&act=ShowLesson&id=<?php echo $this->lesson_next->lesson_id; ?>"><button><?php echo $this->lesson_next->lesson_name; ?> </button></a>
 <?php endif; ?>
