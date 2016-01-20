afc1cee4a1ba0b847cabbfbbe4ff9ff58809b3b6
MartinScorsese <eduardzoz@yandex.ru>
2016-01-20 21:36:39 +0300

    Обновленная страница авторизации
    Input'ы у формы входа стали округлённые, добавлено описание о сайте перед формой входа. (Естественно, изменен css-файл)

--- views/auth/auth.php
+++ views/auth/auth.php
@@ -1,4 +1,12 @@
+<div class="regramka" style="border-radius:7px;">
+    <h1>О компании</h1>
+    <p>
+        Интеллектуальный клуб. Основан в 2015 году.
+    </p>
+</div>
+
 <form method="post" class="forminput" action="?ctrl=Auth&act=Login" role="form">
+    
     <ul>
             <li><label for="inputEmail">Логин:</label></li>
             <li><input type="text" maxlength="50" id="login" name="login"></li>
@@ -12,6 +20,4 @@
     </ul>
 </form>
 
-<div class="regramka">
-    Для регистрации в системе обращайтесь по адресу: ak.alz@mail.ru.
-</div>
+<div class="regramka" style="margin-top: 50px;">Для регистрации в системе обращайтесь по адресу: ak.alz@mail.ru.</div>
